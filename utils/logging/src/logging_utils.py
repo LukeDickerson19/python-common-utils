16c2af4ff44dc254342046e55eb7dd38f868e07f
Luke Dickerson <lucius.dickerson@gmail.com>
2025-09-08 17:10:55 -0400

    updated logging to support indentation w/ log print timestamp

--- utils/logging/src/logging_utils.py
+++ utils/logging/src/logging_utils.py
@@ -38,7 +38,7 @@ class Log:
             indent ................. string .... what an indent looks like in the log
             prepend_datetime_fmt ... string .... format specifying datetime to prepend to each line printed
             timezone ............... string .... timezone to use if prepend_datetime_fmt is not an empty string
-            prepend_memory_usage ... boolean ... 
+            prepend_memory_usage ... boolean ... prepend the memory used and allocated to the python program
 
         Returns:
             Nothing
@@ -47,7 +47,7 @@ class Log:
     def __init__(
         self,
         log_filepath=None,
-        output_to_console=True,
+        output_to_console=False,
         output_to_logfile=True,
         clear_old_log=True,
         indent='|   ',
@@ -91,12 +91,15 @@ class Log:
                 ^^^ very useful for organizing log ^^^
 
         Arguments:
-            string ........... string .... what will be printed
-            i ................ int ....... number of indents to put in front of the string
-            ns ............... boolean ... print a new line in before the string
-            ne ............... boolean ... print a new line in after the string
-            d ................ boolean ... draw a line on the blank line before or after the string
-            end .............. string .... last character(s) to print at the end of the string
+            string ................. string .... what will be printed
+            i ...................... int ....... number of indents to put in front of the string
+            ns ..................... boolean ... print a new line in before the string
+            ne ..................... boolean ... print a new line in after the string
+            oc ..................... boolean ... default to self.output_to_console bool if oc == None else oc
+            of ..................... boolean ... default to self.output_to_logfile bool if of == None else of
+            d ...................... boolean ... draw a line on the blank line before or after the string
+            overwrite_prev_print ... boolean ... overwrite previous print statement in console, does nothing in logfile
+            end .................... string .... last character(s) to print at the end of the string
 
         Returns:
             tuple:
@@ -110,10 +113,11 @@ class Log:
         i=0, # i = number of indents
         ns=False, # ns = newline start
         ne=False, # ne = newline end
+        oc=None, # oc = output to console
+        of=None, # of = output to file
         d=False, # d = draw line
+        overwrite_prev_print=False,
         end='\n'):
-
-        self.same_line_string = False # used for print_same_line(), disregard
 
         # only accept 'string' as a string type
         string = str(string)
@@ -122,7 +126,15 @@ class Log:
         #     raise TypeError("first argument of Log.print() must be of type \'string\', not type \'%s\'" % t)
 
         console_str, logfile_str = None, None
-        if self.output_to_console:
+        output_to_console = self.output_to_console if oc == None else oc
+        if output_to_console:
+            if overwrite_prev_print and self.prev_string != '':
+                sys.stdout.flush()
+                # Move cursor up for each line and clear it
+                for _ in self.prev_string.split('\n'):
+                    # Move cursor up one line
+                    sys.stdout.write("\033[F")   # Cursor up 1 line
+                    sys.stdout.write("\033[K")   # Clear line
             console_str = \
                 self.get_formatted_string(
                     string,
@@ -136,7 +148,8 @@ class Log:
                 end=end,
                 file=sys.stdout)
             self.prev_string = console_str # used from print_prev_line(), disregard
-        if self.output_to_logfile:
+        output_to_logfile = self.output_to_logfile if of == None else of
+        if output_to_logfile:
             logfile_str = \
                 self.get_formatted_string(
                     string,
@@ -165,6 +178,8 @@ class Log:
             i ................ int ....... number of indents to put in front of the string
             ns ............... boolean ... print a new line in before the string
             ne ............... boolean ... print a new line in after the string
+            oc ............... boolean ... default to self.output_to_console bool if oc == None else oc
+            of ............... boolean ... default to self.output_to_logfile bool if of == None else of
             d ................ boolean ... draw a line on the blank line before or after the string
             end .............. string .... last character(s) to print at the end of the string
 
@@ -182,6 +197,8 @@ class Log:
         i=0, # i = number of indents
         ns=False, # ns = newline start
         ne=False, # ne = newline end
+        oc=None, # oc = output to console
+        of=None, # of = output to file
         d=False, # d = draw line
         end='\n'):
 
@@ -213,68 +230,18 @@ class Log:
             i=i,
             ns=ns,
             ne=ne,
+            oc=oc,
+            of=of,
             d=d,
             end=end)
-
-    ''' print_same_line()
-
-        Description:
-            print over the same line as the previous time print_same_line() was called.
-            NOTE:
-                It only seems to work on linux. On windows it prints
-                to the next line instead of the same line
-
-        Arguments:
-            string ........... string .... what will be printed
-            i ................ int ....... number of indents to put in front of the string
-            ns ............... boolean ... print a new line in before the string
-            ne ............... boolean ... print a new line in after the string
-            d ................ boolean ... draw a line on the blank line before or after the string
-            end .............. string .... last character(s) to print at the end of the string
-
-        Returns:
-            tuple:
-                console_str ... string ... string that was output to the console
-                logfile_str ... string ... string that was output to the logfile
-
-        '''
-    def print_same_line(
-        self,
-        string,
-        i=0, # i = number of indents
-        ns=False, # ns = newline start
-        ne=False, # ne = newline end
-        d=False, # d = draw line
-        end='\n'):
-
-        # clear previous text by overwriting non-spaces with spaces
-        if self.same_line_string:
-            # last_line_index = len(self.blanked_out_previous_string) - 1 # this variable isn't used anywhere
-            for line in self.blanked_out_previous_string:
-                print(line, end='')
-
-        # print the string
-        console_str, logfile_str = self.print(
-            string,
-            i=i,
-            ns=ns,
-            ne=ne,
-            d=d,
-            end=end)
-
-        # update blanked_out_previous_string
-        self.same_line_string = True
-        self.blanked_out_previous_string = \
-            ['\x1b[A' + '\r' + ' '*len(line) + '\r' \
-                for line in console_str.split('\n')]
-
-        return console_str, logfile_str
 
     ''' get_formatted_string()
 
         Description:
             create a string with string0, indent0, and other optional arguments that is concatenated properly for printing.
             it also prepends the datetime and the memory usage if the user requested it
+
+            WARNING: this function is honestly some of the worst spagetti I've ever written, but the log is gorgeous!
 
         Arguments:
             string0 .......... string .... what will be printed
@@ -300,18 +267,47 @@ class Log:
         total_indent0 = ''.join([indent0] * i)
         total_indent1 = ''.join([indent0] * (i + 1))
         string = ''
-        if ns:
-            string += (total_indent1 if d else total_indent0) + '\n'
-        for s in string0.split('\n'):
-            p = ''
-            if self.prepend_memory_usage:
-                bytes_used, bytes_allocated = tracemalloc.get_traced_memory()
-                p += f'({Log.convert_bytes(bytes_used)} used {Log.convert_bytes(bytes_allocated)} allocated)'.ljust(45, ' ') # pad w/ spaces
+        div_mark = '-'
+        mock_indent = ' '
+        max_estimated_indents = 10
+        assert i <= max_estimated_indents
+        prepend_sep = ' ' * (max_estimated_indents + 1) # set it to a number larger than the estimated max number of indents you'll ever use while logging
+        p0 = '' # p0 = mock indents
+        p = '' # p = prepended info after mock indents
+        prepend_stuff = self.prepend_datetime_fmt != '' or self.prepend_memory_usage
+        if prepend_stuff:
             if self.prepend_datetime_fmt != '':
                 now = datetime.now(ZoneInfo(self.timezone))
-                p += now.strftime(self.prepend_datetime_fmt) + ' '
-            string += p + total_indent0 + s + '\n'
+                p += now.strftime(self.prepend_datetime_fmt) + '  '
+            if self.prepend_memory_usage:
+                bytes_used, bytes_allocated = tracemalloc.get_traced_memory()
+                if self.prepend_datetime_fmt != '':
+                    p += f'{div_mark}  '
+                p += f'{Log.convert_bytes(bytes_used)} used {Log.convert_bytes(bytes_allocated)} allocated'.ljust(45, ' ') # pad w/ spaces
+            p0 = mock_indent*i + div_mark
+            p = prepend_sep[:len(prepend_sep) - len(mock_indent*i)] + p + f'{div_mark}  ' # put small 1 space indents before everything so VS Code's code folding features continues to work when there's prepended info such as memory or datetime
+        blank_p = p0 + ' ' * (len(p) - (len(div_mark) + 2)) + f'{div_mark}  ' # blank_p = p but w/ prepend info removed, only marks remain
+        if ns:
+            # print(1, string, 1)
+            string += blank_p if prepend_stuff else ''
+            # print(2, string, 2)
+            string += (total_indent1 if d else total_indent0) + '\n'
+            # print(3, string, 3)
+        for s in string0.split('\n'):
+            if prepend_stuff:
+                if s == '':
+                    string += blank_p
+                else:
+                    string += p0 + p
+            else:
+                string += ''
+            if s == '':
+                total_indent = total_indent1 if d else total_indent0
+            else:
+                total_indent = total_indent0
+            string += total_indent + s + '\n'
         if ne:
+            string += blank_p if prepend_stuff else ''
             string += (total_indent1 if d else total_indent0) + '\n'
         string = string[:-1] # remove final newline character
         return string
