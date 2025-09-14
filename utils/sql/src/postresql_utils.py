a6d1b4acbf247b34f292f808f31d0aa309dc44eb
Luke Dickerson <lucius.dickerson@gmail.com>
2025-09-14 19:34:58 -0400

    final updates

--- utils/sql/src/postresql_utils.py
+++ utils/sql/src/postresql_utils.py
@@ -1,12 +1,3 @@
-# LIBRARIES
-
-# import standard libraries
-import os
-import sys
-import pathlib
-
-# import non-standard libraries
-import numpy as np
 import pandas as pd
 MAX_ROWS = 10
 pd.set_option('display.max_rows', MAX_ROWS)
@@ -14,20 +5,8 @@ pd.set_option('display.max_columns', 20)
 pd.set_option('display.max_colwidth', 200)
 pd.set_option('display.width', 1000)
 import psycopg2 # PostgreSQL database connector
-from datetime import datetime
-
-# import common utils
-COMMON_UTILS_REPO_PATH = str(pathlib.Path(__file__).resolve().parent.parent.parent.parent)
-LOG_UTIL_PATH          = os.path.join(COMMON_UTILS_REPO_PATH, 'utils', 'logging', 'src')
-sys.path.append(LOG_UTIL_PATH)
-import logging_utils
-# print('COMMON_UTILS_REPO_PATH\t', COMMON_UTILS_REPO_PATH)
-# print('LOG_UTIL_PATH\t\t', LOG_UTIL_PATH)
-# sys.exit()
 
 
-
-# CONSTANTS
 
 # Connect to PostgreSQL
 # default database is 'postgres'
